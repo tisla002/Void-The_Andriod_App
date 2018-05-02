935773605e464b86616d651d4361d08ea8491a25
danay001 <danay001@ucr.edu>
2018-05-02 01:48:58 -0700

    Implemented uploading of new profile image

--- app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
+++ app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
@@ -1,9 +1,13 @@
 package com.example.starw.chatapp;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,14 +16,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 
 public class ChangeProfileImage extends AppCompatActivity {
 
     ImageView profileImageImgView;
-    Button changeImage;
+    Button changeImage, upload;
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://void-app-5369d.appspot.com");
+    Uri filePath;
+    ProgressDialog pd;
 
     String imageURL = "https://firebasestorage.googleapis.com/v0/b/void-app-5369d.appspot.com/o/profile_images%2Fno_user.png?alt=media&token=7480d07a-e9cd-41d5-ad7e-63e95624a66e";
 
@@ -34,18 +50,17 @@ public class ChangeProfileImage extends AppCompatActivity {
         profileImageImgView = (ImageView) findViewById(R.id.profileImage);
         Picasso.get().load(imageURL).into(profileImageImgView);
         changeImage = (Button) findViewById(R.id.changeImageBtn);
+        upload = (Button) findViewById(R.id.uploadbtn);
+
+        pd = new ProgressDialog(this);
+        pd.setMessage("Uploading....");
 
         verifyPermissions();
-    }
+        uploadImg();
 
-    public void onActivityResult(int reqCode, int resCode, Intent data) {
-        if (resCode == RESULT_OK) {
-            if (reqCode == 1) {
-                profileImageImgView.setImageURI(data.getData());
-            }
-        }
-    }
 
+
+    }
 
     private void verifyPermissions(){
         Log.d(TAG, "verifyPermissions: asking user for permissions");
@@ -78,6 +93,55 @@ public class ChangeProfileImage extends AppCompatActivity {
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), 1);
 
+            }
+        });
+
+    }
+    public void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (reqCode == 1 && resCode == RESULT_OK && data != null && data.getData() != null) {
+            profileImageImgView.setImageURI(data.getData());
+            filePath = data.getData();
+        }
+        try {
+            //getting image from gallery
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+            //Setting image to ImageView
+            profileImageImgView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void uploadImg(){
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(filePath != null) {
+                    pd.show();
+
+                    StorageReference childRef = storageRef.child("Profile_Img").child("profile.jpg");
+
+                    //uploading the image
+                    UploadTask uploadTask = childRef.putFile(filePath);
+
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            pd.dismiss();
+                            Toast.makeText(ChangeProfileImage.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(ChangeProfileImage.this, "Upload Failed -> " + e, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(ChangeProfileImage.this, "Select an image", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
