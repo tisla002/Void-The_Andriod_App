e9b8176b43cadf2292f9050051bdd02ac19227fa
danay001 <danay001@ucr.edu>
2018-05-01 20:51:29 -0700

    Implemented checking for permissions

--- app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
+++ app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
@@ -2,14 +2,16 @@ package com.example.starw.chatapp;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import static android.app.Activity.RESULT_OK;
-import static android.content.ContentValues.TAG;
 
 
 public class ChangeProfileImage extends AppCompatActivity {
@@ -17,12 +19,50 @@ public class ChangeProfileImage extends AppCompatActivity {
     ImageView profileImageImgView;
     Button changeImage;
 
+    private static final String TAG = "ChangeImageActivity";
+    private static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile_image);
 
 
+        verifyPermissions();
+    }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (resCode == RESULT_OK) {
+            if (reqCode == 1) {
+                profileImageImgView.setImageURI(data.getData());
+            }
+        }
+    }
+
+
+    private void verifyPermissions(){
+        Log.d(TAG, "verifyPermissions: asking user for permissions");
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED)
+        {
+            setNewImage();
+        }else{
+            ActivityCompat.requestPermissions(ChangeProfileImage.this,
+                    permissions,
+                    REQUEST_CODE);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
+    }
+
+    private void setNewImage(){
         profileImageImgView = (ImageView) findViewById(R.id.profileImage);
         changeImage = (Button) findViewById(R.id.changeImageBtn);
         changeImage.setOnClickListener(new View.OnClickListener() {
@@ -35,14 +75,9 @@ public class ChangeProfileImage extends AppCompatActivity {
 
             }
         });
+
     }
 
-    public void onActivityResult(int reqCode, int resCode, Intent data) {
-        if (resCode == RESULT_OK) {
-            if (reqCode == 1) {
-                profileImageImgView.setImageURI(data.getData());
-            }
-        }
-    }
+
 
 }
