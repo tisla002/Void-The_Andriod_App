9216510ecc2f394a926ddd998e7f60a813aab6d4
tyler303047 <tyler303047@yahoo.com>
2018-05-01 17:40:03 -0700

    changed skeleton code to not cause dependency issues

--- app/src/main/java/com/example/starw/chatapp/UserEdit.java
+++ app/src/main/java/com/example/starw/chatapp/UserEdit.java
@@ -1,6 +1,5 @@
 package com.example.starw.chatapp;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,20 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.firebase.client.Firebase;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class User_Edit extends AppCompatActivity {
+public class UserEdit extends AppCompatActivity {
     EditText passwordChange;
     Button saveButton;
     String user, pass;
@@ -42,7 +31,7 @@ public class User_Edit extends AppCompatActivity {
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(User_Edit.this, Users.class));
+                startActivity(new Intent(UserEdit.this, Users.class));
             }
         });
     }
