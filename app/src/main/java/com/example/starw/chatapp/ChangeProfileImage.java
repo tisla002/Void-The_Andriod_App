6fa029b9290f515736a19ffa3bed876760d46705
danay001 <danay001@ucr.edu>
2018-05-01 22:41:47 -0700

    Updated to use image from URL

--- app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
+++ app/src/main/java/com/example/starw/chatapp/ChangeProfileImage.java
@@ -13,11 +13,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 
 public class ChangeProfileImage extends AppCompatActivity {
 
     ImageView profileImageImgView;
     Button changeImage;
+
+    String imageURL = "https://firebasestorage.googleapis.com/v0/b/void-app-5369d.appspot.com/o/profile_images%2Fno_user.png?alt=media&token=7480d07a-e9cd-41d5-ad7e-63e95624a66e";
 
     private static final String TAG = "ChangeImageActivity";
     private static final int REQUEST_CODE = 1;
@@ -27,6 +31,9 @@ public class ChangeProfileImage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile_image);
 
+        profileImageImgView = (ImageView) findViewById(R.id.profileImage);
+        Picasso.get().load(imageURL).into(profileImageImgView);
+        changeImage = (Button) findViewById(R.id.changeImageBtn);
 
         verifyPermissions();
     }
@@ -63,8 +70,6 @@ public class ChangeProfileImage extends AppCompatActivity {
     }
 
     private void setNewImage(){
-        profileImageImgView = (ImageView) findViewById(R.id.profileImage);
-        changeImage = (Button) findViewById(R.id.changeImageBtn);
         changeImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
