c07f9fa79495ee1e5dafc2a53ca86d72db7567f1
tyler303047 <tyler303047@yahoo.com>
2018-05-01 17:38:43 -0700

    changed scaling to look more appealing and have larger window for image

--- app/src/main/java/com/example/starw/chatapp/UserEdit.java
+++ app/src/main/java/com/example/starw/chatapp/UserEdit.java
@@ -1,5 +1,6 @@
 package com.example.starw.chatapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.firebase.client.Firebase;
 
-public class UserEdit extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User_Edit extends AppCompatActivity {
     EditText passwordChange;
     Button saveButton;
     String user, pass;
@@ -31,7 +42,7 @@ public class UserEdit extends AppCompatActivity {
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(UserEdit.this, Users.class));
+                startActivity(new Intent(User_Edit.this, Users.class));
             }
         });
     }
