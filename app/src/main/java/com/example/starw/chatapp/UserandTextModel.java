74c4d13659d347f0ba891102b4874f43ca22734f
Adrian Monges <among003@ucr.edu>
2018-05-03 02:41:58 -0700

    Real Time Chat sending and viewing working!

---
+++ app/src/main/java/com/example/starw/chatapp/UserandTextModel.java
@@ -0,0 +1,25 @@
+package com.example.starw.chatapp;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class UserandTextModel{
+    public String user;
+    public String text;
+
+    public UserandTextModel(){
+        //default, Snape Kills Dumbledore
+    }
+
+    public UserandTextModel(String username, String message){
+
+        this.user = username;
+        this.text = message;
+
+
+    }
+
+
+
+
+}
