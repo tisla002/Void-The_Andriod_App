3f09fd3a002e512dd63fef042cb25a4e02f5b228
MrFlynn <nick@pleatsikas.me>
2018-05-02 22:25:33 -0700

    Refactored thread screen to use Firebase methods.
    Added thread parsing from Firebase and altered click listener to
    send user and thread data to Chat activity.

--- app/src/main/java/com/example/starw/chatapp/Login.java
+++ app/src/main/java/com/example/starw/chatapp/Login.java
@@ -66,8 +66,6 @@ public class Login extends AppCompatActivity {
                     pd.setMessage("Loading...");
                     pd.show();
 
-                    Models users = new Models(user, pass);
-
                     user_db.addListenerForSingleValueEvent(
                             new com.google.firebase.database.ValueEventListener() {
                                 @Override
@@ -79,18 +77,18 @@ public class Login extends AppCompatActivity {
                                         String name = data.child("username").getValue(String.class);
                                         String password = data.child("password").getValue(String.class);
 
-                                        if(name.compareTo(user) == 0) {
+                                        if (name.compareTo(user) == 0) {
                                             login_user = true;
 
                                             Toast.makeText(Login.this,
                                                     "Username Exists.",
                                                     Toast.LENGTH_LONG).show();
 
-                                            if(password.compareTo(pass) == 0){
+                                            if (password.compareTo(pass) == 0) {
                                                 login_pass = true;
 
                                                 break;
-                                            }else{
+                                            } else {
                                                 Toast.makeText(Login.this,
                                                         "Username or Password Does Not Exist.",
                                                         Toast.LENGTH_LONG).show();
@@ -99,8 +97,7 @@ public class Login extends AppCompatActivity {
                                             break;
 
 
-                                        }
-                                        else{
+                                        } else {
                                             Toast.makeText(Login.this,
                                                     "Username or Password Does Not Exist.",
                                                     Toast.LENGTH_LONG).show();
@@ -108,28 +105,10 @@ public class Login extends AppCompatActivity {
 
                                     }
 
-//                                    for (DataSnapshot data : dataSnapshot.getChildren()) {
-//                                        String name = data.child("password").getValue(String.class);
-//
-//                                        if(name.compareTo(pass) == 0){
-//
-//                                            login_pass = true;
-//
-//                                            Toast.makeText(Login.this,
-//                                                    "Password Exists.",
-//                                                    Toast.LENGTH_LONG).show();
-//
-//                                            break;
-//                                        }else{
-//                                            Toast.makeText(Login.this,
-//                                                    "Username or Password Does Not Exist.",
-//                                                    Toast.LENGTH_LONG).show();
-//                                        }
-//
-//                                    }
-
                                     if(login_pass == true && login_user == true){
-                                        startActivity(new Intent(Login.this, Users.class));
+                                        Intent main = new Intent(Login.this, Users.class);
+                                        main.putExtra("username", user);
+                                        startActivity(main);
                                     }
 
 
--- app/src/main/java/com/example/starw/chatapp/Users.java
+++ app/src/main/java/com/example/starw/chatapp/Users.java
@@ -8,36 +8,39 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Users extends AppCompatActivity {
     ListView usersList;
     TextView noUsersText;
-    ArrayList<String> al = new ArrayList<>();
-    int totalUsers = 0;
-    ProgressDialog pd;
+
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance(
+            "https://void-app-5369d.firebaseio.com/");
+    private final DatabaseReference thread_db = database.getReference();
+
+    private ArrayList<String> threads = new ArrayList<>();
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        Intent intent = getIntent();
+        final String username = intent.getStringExtra("username");
 
         usersList = (ListView)findViewById(R.id.usersList);
         noUsersText = (TextView)findViewById(R.id.noUsersText);
@@ -46,28 +49,39 @@ public class Users extends AppCompatActivity {
         pd.setMessage("Loading...");
         pd.show();
 
-        String url = "https://void-app-5369d.firebaseio.com/.json";
+        thread_db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    if (data.getRef().getParent().getKey().compareTo("threads") == 0) {
+                        threads.add(data.getKey());
+                    }
+                }
 
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
-            @Override
-            public void onResponse(String s) {
-                doOnSuccess(s);
+                doOnSuccess(threads);
             }
-        },new Response.ErrorListener(){
+
             @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError);
-            }
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
         });
-
-        RequestQueue rQueue = Volley.newRequestQueue(Users.this);
-        rQueue.add(request);
 
         usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                UserDetails.chatWith = al.get(position);
-                startActivity(new Intent(Users.this, Chat.class));
+                Intent chat = new Intent(Users.this, Chat.class);
+                chat.putExtra("thread_id", threads.get(position));
+                chat.putExtra("username", username);
+
+                startActivity(chat);
             }
         });
     }
@@ -93,39 +107,21 @@ public class Users extends AppCompatActivity {
                 startActivity(new Intent(Users.this, Login.class));
                 break;
         }
-        //return super.onOptionsItemSelected(item);
+
         return true;
     }
 
-    public void doOnSuccess(String s){
-        try {
-            JSONObject obj = new JSONObject(s);
-
-            Iterator i = obj.keys();
-            String key = "";
-
-            while(i.hasNext()){
-                key = i.next().toString();
-
-                if(!key.equals(UserDetails.username)) {
-                    al.add(key);
-                }
-
-                totalUsers++;
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        if(totalUsers <=1){
+    public void doOnSuccess(ArrayList<String> t) {
+        if (t.size() == 0) {
             noUsersText.setVisibility(View.VISIBLE);
             usersList.setVisibility(View.GONE);
-        }
-        else{
+        } else {
             noUsersText.setVisibility(View.GONE);
             usersList.setVisibility(View.VISIBLE);
-            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+
+            usersList.setAdapter(new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    t));
         }
 
         pd.dismiss();
