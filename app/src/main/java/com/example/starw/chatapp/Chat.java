acf43dbae029bf8073363a3e96374af8da4d1383
MrFlynn <nick@pleatsikas.me>
2018-05-06 14:55:45 -0700

    Migrated Chat.java to use Firebase auth.

--- app/src/main/java/com/example/starw/chatapp/Chat.java
+++ app/src/main/java/com/example/starw/chatapp/Chat.java
@@ -1,11 +1,9 @@
 package com.example.starw.chatapp;
 
-//comment test
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,115 +12,88 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
-
 public class Chat extends AppCompatActivity {
     LinearLayout layout;
     ImageView sendButton;
     EditText messageArea;
     ScrollView scrollView;
-    //Firebase reference1, reference2;
+
+    private String username;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        layout = (LinearLayout) findViewById(R.id.layout1);
-        sendButton = (ImageView) findViewById(R.id.sendButton);
-        messageArea = (EditText) findViewById(R.id.messageArea);
-        scrollView = (ScrollView) findViewById(R.id.scrollView);
+        layout = findViewById(R.id.layout1);
+        sendButton = findViewById(R.id.sendButton);
+        messageArea = findViewById(R.id.messageArea);
+        scrollView = findViewById(R.id.scrollView);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final Intent intent = getIntent();
-        final String username = intent.getStringExtra("username");
         final String thread_id = intent.getStringExtra("thread_id");
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance("https://void-app-5369d.firebaseio.com/");
-        final DatabaseReference dataRef = database.getReference().child("threads").child(thread_id).child("messages");
-       // final DatabaseReference dataRef2 = database.getReference().child("threads").child(thread_id).child("users");
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference dataRef = database.getReference()
+                .child("threads")
+                .child(thread_id)
+                .child("messages");
 
-        //reference1 = new Firebase("https://void-app-5369d.firebaseio.com/" + UserDetails.username + "_" + UserDetails.chatWith);
-        //reference2 = new Firebase("https://void-app-5369d.firebaseio.com/" + UserDetails.chatWith + "_" + UserDetails.username);
+        final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final DatabaseReference users = database.getReference()
+                .child("users");
+
+        users.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.getRef().getKey().compareTo(uid) == 0) {
+                    username = dataSnapshot.child("username").getValue(String.class);
+                    Log.d("STATE", username);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String messageText = messageArea.getText().toString();
 
                 if (!messageText.equals("")) {
-                    /*HashMap<String, String> mess = new HashMap<String, String>();
-                    HashMap<String, HashMap<String, String>> map = new HashMap<String, HashMap<String, String>>();
-                    HashMap<String, String> users = new HashMap<String, String>();
-
-                    users.put("user", UserDetails.chatWith);
-                    mess.put("user", UserDetails.username);
-                    mess.put("text", messageText);
-                    map.put("user", users);
-                    map.put("message", mess);*/
-
-                    UserandTextModel pushUser = new UserandTextModel(intent.getStringExtra("username"), messageText);
-
-
-
+                    UserandTextModel pushUser = new UserandTextModel(username, messageText);
                     dataRef.push().setValue(pushUser);
-                  //  dataRef2.push().setValue(pushUser.user);
                     messageArea.setText("");
                 }
             }
-
-
-
         });
-
-        dataRef.addListenerForSingleValueEvent(
-                new com.google.firebase.database.ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        for (DataSnapshot data : dataSnapshot.getChildren()) {
-                            String x = data.child("text").getValue(String.class);//child("messages").getValue(String.class); .child("text")
-
-                            String sender = data.child("user").getValue(String.class);//child("messages").getValue(String.class); .child("user")
-
-
-                            if (sender.equals(UserDetails.username)) {
-                                addMessageBox("You:\n" + x, 1);
-                            } else {
-                                addMessageBox(sender + ":\n" + x, 2);
-                            }
-                        }
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
 
         dataRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-                String x = dataSnapshot.child("text").getValue(String.class);//child("messages").getValue(String.class); .child("text")
+                String x = dataSnapshot.child("text").getValue(String.class);
+                String sender = dataSnapshot.child("user").getValue(String.class);
 
-                String sender = dataSnapshot.child("user").getValue(String.class);//child("messages").getValue(String.class); .child("user")
-
-
-                if (sender.equals(UserDetails.username)) {
+                if (sender.compareTo(username) == 0) {
                     addMessageBox("You:\n" + x, 1);
                 } else {
                     addMessageBox(sender + ":\n" + x, 2);
@@ -162,7 +133,10 @@ public class Chat extends AppCompatActivity {
     public void addMessageBox(String message, int type) {
         TextView textView = new TextView(Chat.this);
         textView.setText(message);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
         lp.setMargins(0, 0, 0, 10);
         textView.setLayoutParams(lp);
 
@@ -175,6 +149,4 @@ public class Chat extends AppCompatActivity {
         layout.addView(textView);
         scrollView.fullScroll(View.FOCUS_DOWN);
     }
-
-
 }
