a26fc9308e3d8277f227a218c2fd2f5a934554c7
MrFlynn <nick@pleatsikas.me>
2018-05-06 15:09:45 -0700

    Altered UserEdit.java to user Firebase auth.

--- app/src/main/java/com/example/starw/chatapp/UserEdit.java
+++ app/src/main/java/com/example/starw/chatapp/UserEdit.java
@@ -3,17 +3,16 @@ package com.example.starw.chatapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class UserEdit extends AppCompatActivity {
     private TextView myName;
@@ -25,43 +24,48 @@ public class UserEdit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        myName = (TextView) findViewById(R.id.Name);
-        myProfileImage = (ImageView) findViewById(R.id.Image);
-        myUserName = (TextView) findViewById(R.id.Username);
+        myName = findViewById(R.id.Name);
+        myProfileImage = findViewById(R.id.Image);
+        myUserName = findViewById(R.id.Username);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://void-app-5369d.firebaseio.com/");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference user_db = database.getReference().child("users");
 
-        user_db.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String uid = user.getUid();
 
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
-                    String name = data.child("firstname").getValue(String.class);
-                    String username = data.child("username").getValue(String.class);
-//                    String picture = data.child("picture").getValue(String.class);
+        user_db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.getRef().getKey().compareTo(uid) == 0) {
+                    String username = dataSnapshot.child("username").getValue(String.class);
+                    String name = user.getDisplayName();
 
                     myName.setText(name);
                     myUserName.setText(username);
                     myProfileImage.setImageResource(R.drawable.no_user);
                 }
-
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
 
-            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
         });
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            //This is the back button
+            // This is the back button
             case android.R.id.home:
                 finish();
                 return true;
